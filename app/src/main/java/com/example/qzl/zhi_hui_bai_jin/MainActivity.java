2e39e4b17a080346a0e271c48bce70a704e472aa
Qzl <2538096489@qq.com>
2016-07-27 20:55:10 +0800

    在主页面引入SlidingMenu实现侧边栏

--- app/src/main/java/com/example/qzl/zhi_hui_bai_jin/MainActivity.java
+++ app/src/main/java/com/example/qzl/zhi_hui_bai_jin/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.qzl.zhi_hui_bai_jin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
 /**
  * 主页面
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SlidingFragmentActivity {
+
+    private SlidingMenu mSlidingMenu;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//取标题
         setContentView(R.layout.activity_main);
+        //设置左侧边栏
+        setBehindContentView(R.layout.menu_left);
+        //获取SlideMenu
+        mSlidingMenu = getSlidingMenu();
+        //设置触摸方式为全屏
+        mSlidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        mSlidingMenu.setBehindOffset(400);//屏幕预留200像素宽度
     }
 }
