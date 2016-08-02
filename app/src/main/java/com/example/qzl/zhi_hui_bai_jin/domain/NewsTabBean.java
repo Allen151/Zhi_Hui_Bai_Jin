591f768dc27ea99ead8f18057e3d1ec10026a143
Qzl <2538096489@qq.com>
2016-08-02 16:39:07 +0800

    .页签详情页结构分析&获取页签页网络数据

---
+++ app/src/main/java/com/example/qzl/zhi_hui_bai_jin/domain/NewsTabBean.java
@@ -0,0 +1,35 @@
+package com.example.qzl.zhi_hui_bai_jin.domain;
+
+import java.util.ArrayList;
+
+/**
+ * 页签详情数据对象
+ * Created by Qzl on 2016-08-02.
+ */
+public class NewsTabBean {
+    public NewsTab data;
+    public class NewsTab{
+        public String more;
+        public ArrayList <NewsData> news;
+        public ArrayList <TopNews> topnews;
+    }
+    //新闻列表对象
+    public class NewsData{
+        public int id;
+        public String listimage;//新闻所对应的图片
+        public String pubdate;
+        public String title;
+        public String type;
+        public String url;
+    }
+
+    //头条新闻对象
+    public class TopNews{
+        public int id;
+        public String topimage;//新闻所对应的图片
+        public String pubdate;
+        public String title;
+        public String type;
+        public String url;
+    }
+}
--- app/src/main/java/com/example/qzl/zhi_hui_bai_jin/implayment/menu/TabDetailPager.java
+++ app/src/main/java/com/example/qzl/zhi_hui_bai_jin/implayment/menu/TabDetailPager.java
@@ -1,38 +1,108 @@
 package com.example.qzl.zhi_hui_bai_jin.implayment.menu;
 
 import android.app.Activity;
-import android.graphics.Color;
-import android.view.Gravity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.qzl.zhi_hui_bai_jin.R;
 import com.example.qzl.zhi_hui_bai_jin.base.BaseMenuDetailPager;
 import com.example.qzl.zhi_hui_bai_jin.domain.NewsMenu;
+import com.example.qzl.zhi_hui_bai_jin.domain.NewsTabBean;
+import com.example.qzl.zhi_hui_bai_jin.global.GlobalConstants;
+import com.google.gson.Gson;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
+import com.lidroid.xutils.view.annotation.ViewInject;
 
 /**
  * Created by Qzl on 2016-08-02.
  */
 public class TabDetailPager extends BaseMenuDetailPager{
     private NewsMenu.NewsTabData mTabData;//单个页签的网络数据
-    private TextView mView;
+//    private TextView mView;
+    @ViewInject(R.id.vp_tab_detail_top_news)
+    private ViewPager mViewPager;
+    private final String mUrl;
 
     public TabDetailPager(Activity activity, NewsMenu.NewsTabData newsTabData) {
         super(activity);
         mTabData = newsTabData;
+
+        mUrl = GlobalConstants.SERVER_URL + mTabData.url;
     }
 
     @Override
     public View initView() {
-        mView = new TextView(mActivity);
-        //view.setText(mTabData.title);//此处空指针
-        mView.setTextColor(Color.RED);
-        mView.setTextSize(22);
-        mView.setGravity(Gravity.CENTER);
-        return mView;
+//        mView = new TextView(mActivity);
+//        //view.setText(mTabData.title);//此处空指针
+//        mView.setTextColor(Color.RED);
+//        mView.setTextSize(22);
+//        mView.setGravity(Gravity.CENTER);
+        View view = View.inflate(mActivity, R.layout.pager_tab_detail,null);
+        ViewUtils.inject(this,view);
+        return view;
     }
 
     @Override
     public void initData() {
-        mView.setText(mTabData.title);
+//        mView.setText(mTabData.title);
+        getDataFromServer();
+    }
+
+    private void getDataFromServer() {
+        HttpUtils httpUtils = new HttpUtils();
+        httpUtils.send(HttpRequest.HttpMethod.GET, mUrl, new RequestCallBack<String>() {
+            @Override
+            public void onSuccess(ResponseInfo<String> responseInfo) {
+                String result = responseInfo.result;
+                processData(result);
+            }
+
+            @Override
+            public void onFailure(HttpException error, String msg) {
+                //请求失败
+                error.printStackTrace();
+                Toast.makeText(mActivity,msg,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    //解析数据
+    private void processData(String result) {
+        Gson gson = new Gson();
+        NewsTabBean newsTabBean = gson.fromJson(result, NewsTabBean.class);
+        
+
+    }
+
+    //头条新闻数据适配器
+    class TopNewsAdapter extends PagerAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            return super.instantiateItem(container, position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
     }
 }
