4090632dd74c9052d0fae47d39f7914702b29ea5
Qzl <2538096489@qq.com>
2016-08-03 11:18:25 +0800

    下拉刷新回调接口

--- app/src/main/java/com/example/qzl/zhi_hui_bai_jin/implayment/menu/TabDetailPager.java
+++ app/src/main/java/com/example/qzl/zhi_hui_bai_jin/implayment/menu/TabDetailPager.java
@@ -34,9 +34,9 @@ import java.util.ArrayList;
 /**
  * Created by Qzl on 2016-08-02.
  */
-public class TabDetailPager extends BaseMenuDetailPager{
+public class TabDetailPager extends BaseMenuDetailPager {
     private NewsMenu.NewsTabData mTabData;//单个页签的网络数据
-//    private TextView mView;
+    //    private TextView mView;
     @ViewInject(R.id.vp_tab_detail_top_news)
     private TopNewsViewPager mViewPager;
 
@@ -69,21 +69,29 @@ public class TabDetailPager extends BaseMenuDetailPager{
 //        mView.setTextColor(Color.RED);
 //        mView.setTextSize(22);
 //        mView.setGravity(Gravity.CENTER);
-        View view = View.inflate(mActivity, R.layout.pager_tab_detail,null);
-        ViewUtils.inject(this,view);
+        View view = View.inflate(mActivity, R.layout.pager_tab_detail, null);
+        ViewUtils.inject(this, view);
 
         //给listView添加头布局
-        View mHeaderView = View.inflate(mActivity,R.layout.list_item_header,null);
-        ViewUtils.inject(this,mHeaderView);//此处必须将头布局也注入
+        View mHeaderView = View.inflate(mActivity, R.layout.list_item_header, null);
+        ViewUtils.inject(this, mHeaderView);//此处必须将头布局也注入
         mLvList.addHeaderView(mHeaderView);
+        //5 前端界面设置回调
+        mLvList.setOnRefreshListener(new PullToRefreshListView.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //属性数据
+                getDataFromServer();
+            }
+        });
         return view;
     }
 
     @Override
     public void initData() {
 //        mView.setText(mTabData.title);
-        String cache = CacheUtils.getCache(mUrl,mActivity);
-        if (cache != null){
+        String cache = CacheUtils.getCache(mUrl, mActivity);
+        if (cache != null) {
             processData(cache);
         }
         getDataFromServer();
@@ -97,14 +105,18 @@ public class TabDetailPager extends BaseMenuDetailPager{
                 String result = responseInfo.result;
                 processData(result);
                 //设置缓存
-                CacheUtils.setCache(mUrl,result,mActivity);
+                CacheUtils.setCache(mUrl, result, mActivity);
+                //收起下拉刷新控件
+                mLvList.onRefreshComplete();
             }
 
             @Override
             public void onFailure(HttpException error, String msg) {
                 //请求失败
                 error.printStackTrace();
-                Toast.makeText(mActivity,msg,Toast.LENGTH_SHORT).show();
+                Toast.makeText(mActivity, msg, Toast.LENGTH_SHORT).show();
+                //收起下拉刷新控件
+                mLvList.onRefreshComplete();
             }
         });
     }
@@ -115,7 +127,7 @@ public class TabDetailPager extends BaseMenuDetailPager{
         NewsTabBean newsTabBean = gson.fromJson(result, NewsTabBean.class);
         //头条新闻填充数据
         mTopNews = newsTabBean.data.topnews;
-        if (mTopNews != null){
+        if (mTopNews != null) {
             mViewPager.setAdapter(new TopNewsAdapter());
             mIndicator.setViewPager(mViewPager);
             mIndicator.setSnap(true);//快照方式展示
@@ -144,15 +156,15 @@ public class TabDetailPager extends BaseMenuDetailPager{
         }
         //列表新闻
         mNewsList = newsTabBean.data.news;
-        if (mNewsList != null){
+        if (mNewsList != null) {
             mNewsAdapter = new NewsAdapter();
             mLvList.setAdapter(mNewsAdapter);
         }
     }
 
     //头条新闻数据适配器
-    class TopNewsAdapter extends PagerAdapter{
-        public TopNewsAdapter(){
+    class TopNewsAdapter extends PagerAdapter {
+        public TopNewsAdapter() {
             mBitmapUtils = new BitmapUtils(mActivity);
             //设置加载中的默认图片
             mBitmapUtils.configDefaultLoadingImage(R.drawable.topnews_item_default);
@@ -176,7 +188,7 @@ public class TabDetailPager extends BaseMenuDetailPager{
             String imageUrl = mTopNews.get(position).topimage;//图片的下载链接
             //下载图片，将图片设置给imageView，避免内存溢出，缓存
             //BitmapUtils->xUtils
-            mBitmapUtils.display(view,imageUrl);
+            mBitmapUtils.display(view, imageUrl);
 
             container.addView(view);
             return view;
@@ -188,11 +200,11 @@ public class TabDetailPager extends BaseMenuDetailPager{
         }
     }
 
-    class NewsAdapter extends BaseAdapter{
+    class NewsAdapter extends BaseAdapter {
 
         private BitmapUtils mBitmapUtils;
 
-        public NewsAdapter(){
+        public NewsAdapter() {
             mBitmapUtils = new BitmapUtils(mActivity);
             mBitmapUtils.configDefaultLoadingImage(R.drawable.news_pic_default);
         }
@@ -215,14 +227,14 @@ public class TabDetailPager extends BaseMenuDetailPager{
         @Override
         public View getView(int position, View convertView, ViewGroup viewGroup) {
             ViewHolder holder;
-            if (convertView == null){
-                convertView = View.inflate(mActivity,R.layout.list_item_news,null);
+            if (convertView == null) {
+                convertView = View.inflate(mActivity, R.layout.list_item_news, null);
                 holder = new ViewHolder();
                 holder.ivIcon = (ImageView) convertView.findViewById(R.id.iv_item_news_icon);
                 holder.tvTitle = (TextView) convertView.findViewById(R.id.tv_item_news_title);
                 holder.tvDate = (TextView) convertView.findViewById(R.id.tv_item_news_data);
                 convertView.setTag(holder);
-            }else {
+            } else {
                 holder = (ViewHolder) convertView.getTag();
             }
             NewsTabBean.NewsData news = getItem(position);
@@ -231,13 +243,13 @@ public class TabDetailPager extends BaseMenuDetailPager{
             //设置时间
             holder.tvDate.setText(news.pubdate);
             //设置图片
-            mBitmapUtils.display(holder.ivIcon,news.listimage);
+            mBitmapUtils.display(holder.ivIcon, news.listimage);
             return convertView;
         }
     }
 
-    static class ViewHolder{
+    static class ViewHolder {
         public ImageView ivIcon;
-        public TextView tvTitle,tvDate;
+        public TextView tvTitle, tvDate;
     }
 }
--- app/src/main/java/com/example/qzl/zhi_hui_bai_jin/view/PullToRefreshListView.java
+++ app/src/main/java/com/example/qzl/zhi_hui_bai_jin/view/PullToRefreshListView.java
@@ -4,7 +4,12 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.qzl.zhi_hui_bai_jin.R;
 
@@ -14,9 +19,22 @@ import com.example.qzl.zhi_hui_bai_jin.R;
  */
 public class PullToRefreshListView extends ListView {
 
+    private static final int STATE_PULL_TO_REFRESH = 1;
+    private static final int STATE_RELEASE_TO_REFRESH = 2;
+    private static final int STATE_REFRESHING = 3;
+
+    private int mCurrentState = STATE_PULL_TO_REFRESH;//当前状态
+
     private View mHeaderView;
     private int mHeaderViewHeight;
     private int mStartY = -1;
+    private TextView mTvTitle;
+    private TextView mTvTime;
+    private ImageView mIvArrow;
+    private RotateAnimation mAnimUp;
+    private RotateAnimation mAnimDown;
+    private ProgressBar mPbProgress;
+
 
     public PullToRefreshListView(Context context) {
         super(context);
@@ -39,10 +57,16 @@ public class PullToRefreshListView extends ListView {
     private void initHeaderView() {
         mHeaderView = View.inflate(getContext(), R.layout.pull_to_refresh, null);
         this.addHeaderView(mHeaderView);
+        mTvTitle = (TextView) mHeaderView.findViewById(R.id.tv_pull_to_reresh_title);
+        mTvTime = (TextView) mHeaderView.findViewById(R.id.tv_pull_to_reresh_time);
+        mIvArrow = (ImageView) mHeaderView.findViewById(R.id.iv_pull_to_refresh_arrow);
+        mPbProgress = (ProgressBar) mHeaderView.findViewById(R.id.pb_pull_to_refresh_loading);
         //隐藏头布局
         mHeaderView.measure(0, 0);
         mHeaderViewHeight = mHeaderView.getMeasuredHeight();
         mHeaderView.setPadding(0, -mHeaderViewHeight, 0, 0);
+        //箭头的动画
+        initAnim();
     }
 
     @Override
@@ -57,6 +81,10 @@ public class PullToRefreshListView extends ListView {
                     // actionDown事件会被viewPager消费掉，导致stareY没有复制，此处需要重新获取一下
                     mStartY = (int) ev.getY();
                 }
+                if (mCurrentState == STATE_REFRESHING){
+                    //如果是正在刷新的状态，直接跳出
+                    break;
+                }
                 int endY = (int) ev.getY();//终点的坐标
                 int dy = endY - mStartY;//偏移量
                 int firstVisiblePosition = getFirstVisiblePosition();//当前显示的第一个item的位置
@@ -64,12 +92,99 @@ public class PullToRefreshListView extends ListView {
                 if (dy > 0 && firstVisiblePosition == 0){
                     int padding = dy - mHeaderViewHeight;//计算当前下拉控件的padding值
                     mHeaderView.setPadding(0,padding,0,0);
+                    if (padding > 0 && mCurrentState != STATE_RELEASE_TO_REFRESH){
+                        //改为松开刷新
+                        mCurrentState = STATE_RELEASE_TO_REFRESH;
+                        refreshState();
+                    }else if (padding < 0 && mCurrentState != STATE_PULL_TO_REFRESH){
+                        //改为下拉刷新
+                        mCurrentState = STATE_PULL_TO_REFRESH;
+                        refreshState();
+                    }
                     return true;
                 }
                 break;
             case MotionEvent.ACTION_UP:
+                mStartY = -1;
+                if (mCurrentState == STATE_RELEASE_TO_REFRESH){
+                    mCurrentState = STATE_REFRESHING;
+                    refreshState();
+                    //完整展示头布局
+                    mHeaderView.setPadding(0,0,0,0);
+                    //4 进行回调
+                    if (mListener != null) {
+                        mListener.onRefresh();
+                    }
+                }else if (mCurrentState == STATE_RELEASE_TO_REFRESH){
+                    //隐藏头布局
+                    mHeaderView.setPadding(0,-mHeaderViewHeight,0,0);
+                }
                 break;
         }
         return super.onTouchEvent(ev);
+    }
+
+    /**
+     * 初始化箭头动画
+     */
+    private void initAnim(){
+        mAnimUp = new RotateAnimation(0,-180, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        mAnimUp.setDuration(200);
+        mAnimUp.setFillAfter(true);//保持状态
+        mAnimDown = new RotateAnimation(-180,0, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        mAnimDown.setDuration(200);
+        mAnimDown.setFillAfter(true);//保持状态
+    }
+
+    /**
+     * 根据当前状态刷新界面
+     */
+    private void refreshState() {
+        switch (mCurrentState){
+            case STATE_PULL_TO_REFRESH://下拉刷新
+                mTvTitle.setText("下拉刷新");
+                mPbProgress.setVisibility(View.INVISIBLE);
+                mIvArrow.setVisibility(View.VISIBLE);
+                mIvArrow.startAnimation(mAnimDown);
+                break;
+            case STATE_RELEASE_TO_REFRESH://松开刷新
+                mTvTitle.setText("松开刷新");
+                mPbProgress.setVisibility(View.INVISIBLE);
+                mIvArrow.setVisibility(View.VISIBLE);
+                mIvArrow.startAnimation(mAnimUp);
+                break;
+            case STATE_REFRESHING://正在刷新
+                mTvTitle.setText("正在刷新...");
+                //清除箭头动画，否则无法隐藏
+                mIvArrow.clearAnimation();
+                mPbProgress.setVisibility(View.VISIBLE);//显示进度条
+                mIvArrow.setVisibility(View.INVISIBLE);//隐藏箭头
+                break;
+        }
+    }
+
+    /**
+     * 刷新结束，收起控件
+     */
+    public void onRefreshComplete(){
+        mHeaderView.setPadding(0,-mHeaderViewHeight,0,0);
+        mCurrentState = STATE_RELEASE_TO_REFRESH;
+        mTvTitle.setText("下拉刷新");
+        mPbProgress.setVisibility(INVISIBLE);
+        mIvArrow.setVisibility(VISIBLE);
+    }
+    //3 定义成员变量，接收监听对象
+    private OnRefreshListener mListener;
+    /**
+     * 2、暴露接口，设置监听
+     */
+    public void setOnRefreshListener(OnRefreshListener listener){
+        mListener = listener;
+    }
+    /**
+     * 1、下拉刷新的回调接口
+     */
+    public interface OnRefreshListener{
+        public void onRefresh();
     }
 }
