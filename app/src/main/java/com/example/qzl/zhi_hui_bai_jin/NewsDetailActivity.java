c60c3c8334db6b60959090af4c4498d1d8a78b58
Qzl <2538096489@qq.com>
2016-08-03 19:09:27 +0800

    创建新闻详情页

---
+++ app/src/main/java/com/example/qzl/zhi_hui_bai_jin/NewsDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.qzl.zhi_hui_bai_jin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 新闻详情页
+ */
+public class NewsDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detail);
+    }
+}
